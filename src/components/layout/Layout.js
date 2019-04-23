acdb27bc1f6af9592e6cc0aeb85cdecc10a3b8c3
Matt Billings <mbillings@gmail.com>
2019-04-23 12:47:23 -0700

    add social meta back into layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,9 +4,12 @@ import styled from 'styled-components';
 import { StaticQuery, graphql } from 'gatsby';
 import { node } from 'prop-types';
 import GlobalStyle from '../styled/GlobalStyle';
-import favicon from '../../static/icons/favicon.png';
-import appleTouchIcon from '../../static/icons/apple-touch-icon.png';
-import SocialMeta from './SocialMeta';
+import socialImage from '../../static/social.jpg';
+
+const socialTitle = 'Matt Billings dot Tech';
+const socialUrl = 'https://mattbillings.tech';
+const socialDescription =
+  'A playground for a reflective developer, hoping to make a positive impact on the world through code.';
 
 const Layout = ({ children }) => {
   return (
@@ -29,13 +32,15 @@ const Layout = ({ children }) => {
               name="viewport"
               content="width=device-width, initial-scale=1.0"
             />
-            <link rel="icon" href={favicon} />
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href={appleTouchIcon}
-            />
-            <SocialMeta />
+            <meta property="og:url" content={socialUrl} />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={socialTitle} />
+            <meta property="og:description" content={socialDescription} />
+            <meta property="og:image" content={`${socialImage}`} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={socialUrl} />
+            <meta name="twitter:description" content={socialDescription} />
+            <meta name="twitter:image" content={socialImage} />
           </Helmet>
           <GlobalStyle />
           <Main>{children}</Main>
